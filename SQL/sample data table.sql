bb2ed5d3d15935b58340933d0b01eb4aeae9d1db
cinalimaster <66794345+cinalimaster@users.noreply.github.com>
2021-05-18 14:17:13 +0300

    Create sample data table.sql

---
+++ SQL/sample data table.sql
@@ -0,0 +1,17 @@
+# this file written by self education purposes may not work on you
+
+SQL Structured Query Language 
+
+SAMPLE DATA
+
+country
+	id	name		population	area
+	1	France		66000000	640000
+	2	Germany		80000000	357000
+	...	...		...		...
+
+city
+	id	name		country_id	populaton	rating
+	1	Paris		1		2200000		5
+	2	Berlin		2		3400000		3
+	...	...		...		...		...
