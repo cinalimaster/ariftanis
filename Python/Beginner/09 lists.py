95499abbac0b22e4f433f5397a89162ea0cda970
cinalimaster <66794345+cinalimaster@users.noreply.github.com>
2021-05-20 14:45:58 +0300

    Update 09 lists.py

--- Python/Beginner/09 lists.py
+++ Python/Beginner/09 lists.py
@@ -2,8 +2,8 @@ friends = ["Kevin", "Karen", "Jim", "Mike", "jane"] # we defined a list
 #             0        1       2       3       4
 print(friends) # prints entire list
 print(friends[0]) # brings kevin
-print(friends[-1]) # counts backwards and gives Jim
-print(friends[2]) #gives jane
+print(friends[-1]) # counts backwards and gives Jane
+print(friends[2]) #gives jim
 print(friends[1:]) # gives starts from 1 and till the end of list
 
 friends[1] = "Mike" # we update information at the list friends position 1 is Mike now
