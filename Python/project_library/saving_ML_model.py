7bd4bcc5a5c73a8774d8a847d1fb840e94f6cfd4
cinalimaster <66794345+cinalimaster@users.noreply.github.com>
2021-06-11 11:37:17 +0300

    Create saving_ML_model.py

---
+++ Python/project_library/saving_ML_model.py
@@ -0,0 +1,32 @@
+# first import libraries
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
+
+linear_regression = LinearRegression()
+linear_regression.fit(xtrain, ytrain)
+
+import pickle
+with open("pickle_model", "wb") as file:
+    pickle.dump(linear_regression, file)
+    
+# use pickle for using saved model to predict values on the test set later
+with open("pickle_model", "rb") as file:
+    model = pickle.load(file)
+
+predictions = model.predict(xtest)
+
+for i in range(len(predictions)):
+    print(predictions[x], xtest[x], [ytest[x]])
