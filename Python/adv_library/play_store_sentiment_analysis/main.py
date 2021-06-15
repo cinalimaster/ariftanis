f004aac03e984890f65a0f162ef78a24b83c460c
cinalimaster <66794345+cinalimaster@users.noreply.github.com>
2021-06-15 13:25:00 +0300

    Create main.py

---
+++ Python/adv_library/play_store_sentiment_analysis/main.py
@@ -0,0 +1,29 @@
+from itertools import count
+from nltk.util import pr
+import pandas as pd
+data = pd.read_csv("user_reviews.csv")
+print(data.head())
+
+# check null
+print(data.isnull().sum())
+
+# drop null
+data = data.dropna()
+print(data.isnull().sum())
+
+# Positive, Negative, and Neutral
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Translated_Review"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Translated_Review"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Translated_Review"]]
+print(data.head())
+
+# scatter plot
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(15, 10))
+sns.scatterplot(data['Sentiment_Polarity'], data['Sentiment_Subjectivity'], 
+                hue = data['Sentiment'], edgecolor='white', palette="twilight_shifted_r")
+plt.title("Google Play Store Reviews Sentiment Analysis", fontsize=20)
+plt.show()
